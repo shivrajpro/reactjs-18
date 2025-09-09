bdfebf277640b51e597b20e619874e644fae6a25
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-09-09 08:09:30 +0530

    create counter context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useContext } from "react";
 import "./App.css";
+import Counter from "./components/Counter";
+import { CounterContext } from "./context/Counter";
 
 
 function App() {
+  const counterState = useContext(CounterContext);
+  console.log("state", counterState);
 
   return (
     <div>
       <h2>Context API in ReactJS</h2>
+      <h3>Counter is {counterState.count}</h3>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
