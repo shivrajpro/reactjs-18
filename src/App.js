745e9f26bbc43bfd79426aa0db387b49010e2d54
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-10-30 23:22:34 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import { useEffect, useState } from "react"
-import useDebounce from "./hooks/useDebounce";
-
 export default function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const debouncedQuery = useDebounce(searchTerm, 2000);
-
-  useEffect(() => {
-    console.log('debouncedQuery', debouncedQuery)
-  }, [debouncedQuery])
-
   return <div>
     <p>React 18 Works</p>
-    <input placeholder="Enter some text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
   </div>
 }
